a081e00e49ba6b31cc2b06721a0adb988ac6dc8b
Agnieszka_KK <agnieszka.korczakkus@gmail.com>
2022-07-07 23:45:21 +0200

    Klienci

--- Klienci.xaml.cs
+++ Klienci.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace Hotel_Kus
 {
@@ -20,14 +21,28 @@ namespace Hotel_Kus
     /// </summary>
     public partial class Klienci : Page
     {
+        Entity_Hotel_Kus context = new Entity_Hotel_Kus();
+        CollectionViewSource clientViewSource;
+        // CollectionViewSource ordViewSource;
         public Klienci()
         {
             InitializeComponent();
+            clientViewSource = ((CollectionViewSource)(FindResource("listaKlientowViewSource")));
+            DataContext = this;
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            context.ListaKlientow.Load();
+
+            clientViewSource.Source = context.ListaKlientow.Local;
+        }
     }
+
 }
